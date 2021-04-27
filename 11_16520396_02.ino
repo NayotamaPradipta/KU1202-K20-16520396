ee2e7dcc8fec0963431de6dd2b5359b9da51a504
NayotamaPradipta <72206534+NayotamaPradipta@users.noreply.github.com>
2021-04-27 15:18:32 +0700

    Update 11_16520396_02.ino

--- 11_16520396_02.ino
+++ 11_16520396_02.ino
@@ -1,19 +1,19 @@
-int sensePin = A0;
-int sensorInput;
-double temp;
+int sensePin = A0;        // pin Arduino yang akan membaca output sensor
+int sensorInput;          // variabel untuk simpan nilai output sensor
+double temp;              // variabel untuk simpan nilai suhu (derajat Celsius)
 
 void setup()
 { 
-  Serial.begin(9600);
+  Serial.begin(9600);     // memulai port serial
 }
 
 void loop()
 {
-  sensorInput = analogRead(sensePin);
-  temp = (double)sensorInput / 1024;
-  temp = temp * 5;
-  temp = temp - 0.5;
-  temp = temp * 100;
+  sensorInput = analogRead(sensePin);   // baca sensor analog
+  temp = (double)sensorInput / 1024;    // hitung persentase dari pembacaan input
+  temp = temp * 5;                      // dikali dengan 5V untuk mendapat nilai tegangan
+  temp = temp - 0.5;                    // dikurangi dengan offset
+  temp = temp * 100;                    // konversi menjadi derajat Ceclius
 
   Serial.print("Suhu: ");
   Serial.print(temp);
